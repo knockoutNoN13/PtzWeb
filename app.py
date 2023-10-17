55e4c849a4daf5eac1e8e681433292afbe49be30
knockoutNoN13 <110716385+knockoutNoN13@users.noreply.github.com>
2023-10-17 17:15:06 +0300

    Added ast

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template, render_template_string, redir
 import requests
 from bs4 import BeautifulSoup
 from werkzeug.datastructures import ImmutableMultiDict
+import ast
+
 
 
 app = Flask(__name__)
@@ -30,7 +32,7 @@ def home():
                 respHead = response.headers
                 respBody = response.text
                 
-                return render_template('new.html', result=respBody, headers=render_template_string(str(respHead)))
+                return render_template('new.html', result=respBody, headers=ast.literal_eval(render_template_string(str(respHead))))
             except Exception as e:
                 return render_template('new.html', err=e)
         else:
@@ -39,25 +41,9 @@ def home():
                 respHead = response.headers
                 respBody = response.text
                 print(respBody, respHead,'hui')
-                return render_template('new.html', result=respBody, headers=render_template_string(str(respHead)))
+                return render_template('new.html', result=respBody, headers=ast.literal_eval(render_template_string(str(respHead))))
             except Exception as e:
                 return render_template('new.html', err=str(e))
-    
-    # elif request.method == 'POST':
-    #     site = request.form['command']
-    #     try:
-    #         response = requests.get(site)
-    #     except Exception as e:
-    #         err = e
-    #         return render_template('new.html', err=err)
-        
-    #     headersDict = response.headers
-    #     headerStrings = []
-    #     for header in headersDict.keys():
-    #         headerStrings.append(': '.join([header, headersDict[header]]))
-    #     headers = '\n'.join(headerStrings)
-    #     respBody = BeautifulSoup(response.text, 'html.parser').prettify()
-    #     return render_template('new.html', result=respBody, headers=render_template_string(headers))
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
